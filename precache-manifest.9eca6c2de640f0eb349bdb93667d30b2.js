8c7ecccc2bac64b85b85f351279f05232a52ae3a
SzubaKrzysztof <szuba.christopher@gmail.com>
2020-07-28 22:41:35 +0200

    Updates

---
+++ precache-manifest.9eca6c2de640f0eb349bdb93667d30b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b467842dd8e10aab05121ea0a5474ce",
+    "url": "/reactMoviesAPI.git/index.html"
+  },
+  {
+    "revision": "976fc8f57a540af692b0",
+    "url": "/reactMoviesAPI.git/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2e7168655c070dd137bc",
+    "url": "/reactMoviesAPI.git/static/css/main.4113915a.chunk.css"
+  },
+  {
+    "revision": "976fc8f57a540af692b0",
+    "url": "/reactMoviesAPI.git/static/js/2.6c91b13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactMoviesAPI.git/static/js/2.6c91b13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7168655c070dd137bc",
+    "url": "/reactMoviesAPI.git/static/js/main.81a7997d.chunk.js"
+  },
+  {
+    "revision": "6aed890d80c316b13f18",
+    "url": "/reactMoviesAPI.git/static/js/runtime-main.ff1b6971.js"
+  }
+]);
